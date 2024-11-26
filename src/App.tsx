9c143f9f226bd4911ed897160d014bda188f866d
pahancha <pahanchathuranga123@gmail.com>
2024-11-26 10:23:29 +0530

    Anonymous signin using firebase.

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect } from 'react'
 import './App.css'
+import { auth } from './firebase-config';
+import { signInAnonymously, onAuthStateChanged } from 'firebase/auth';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    signInAnonymously(auth);
+    onAuthStateChanged(auth, user => {
+      if (user) {
+        console.log("User signed in ", user.uid);
+      }
+    })
+  },[])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App'>
+      <header>
+        <h1> Docs Pro </h1>
+      </header>
+    </div>
   )
 }
 
